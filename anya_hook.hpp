8b661596b2d1f3047e5bfde54fa2deb8f922ed58
Rexi <68111233+RexiRexii@users.noreply.github.com>
2022-09-26 17:10:27 +0300

    Update anya_hook.hpp
    update for shitass

--- anya_hook.hpp
+++ anya_hook.hpp
@@ -8,18 +8,19 @@
 class anya_hook
 {
 public:
-	std::uintptr_t target_function; // detour
-
 	std::uint8_t* function_o; // old
 	std::uint8_t* function_t; // backup
 
+	std::uintptr_t context; // detour
+
 	std::size_t function_size;
+	bool allocate;
 
 public:
 	explicit anya_hook();
 
-	void detour(const std::uintptr_t to_hook, const std::uintptr_t to_replace, const std::size_t length);
-	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace, std::int32_t instr_nops = 0);
+	void detour(const std::uintptr_t to_hook, const std::uintptr_t to_replace, const std::size_t length, bool restore = true);
+	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace, std::int32_t instr_nops = 0, bool restore = true);
 
 	void unhook(std::uintptr_t to_unhook);
 	void yield(const std::uintptr_t to_yield);
