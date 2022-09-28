2ed25da557145f7ed9211f4d5f0018bee31130ba
Rexi <68111233+RexiRexii@users.noreply.github.com>
2022-09-28 15:45:43 +0300

    bug fix

--- anya_hook.hpp
+++ anya_hook.hpp
@@ -13,14 +13,11 @@ public:
 
 	std::uintptr_t context; // detour
 
-	std::size_t function_size;
-	bool allocate;
+	std::size_t function_length;
 
 public:
 	explicit anya_hook();
-
-	void detour(const std::uintptr_t to_hook, const std::uintptr_t to_replace, const std::size_t length, bool restore = true);
-	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace, std::int32_t instr_nops = 0, bool restore = true);
+	std::uintptr_t hook(const std::uintptr_t to_hook, const std::uintptr_t to_replace);
 
 	void unhook(std::uintptr_t to_unhook);
 	void yield(const std::uintptr_t to_yield);
